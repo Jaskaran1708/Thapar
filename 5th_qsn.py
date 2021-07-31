349fa947d6fa9eef080c6752692167a5006d1e91
Jaskaran <sainijass2001@gmail.com>
2021-07-31 18:56:48 +0530

    count of a number

---
+++ 5th_qsn.py
@@ -0,0 +1,4 @@
+from array import *
+array_num = array( 'i', [4,5,2,65,4,12,4,56, 98,4, 23])
+print("Original array: "+str(array_num))
+print("Number of occurrences of the number 4 in the said array: "+str(array_num.count(4)))
